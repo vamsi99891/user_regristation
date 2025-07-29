edce922baac3906d262d359b9060e892be04038b
vamsi kota <vamsi.kota@thezennialpro.com>
2025-07-29 17:23:14 +0530

    added user logs

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,9 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import ASCENDING
 
-client = AsyncIOMotorClient("mongodb+srv://vamsikota3545:<db_password>@resumestore.av7rqbr.mongodb.net/")
-db = client["user_registration_model"]
-users_collection = db["users"]
+client = AsyncIOMotorClient("mongodb+srv://vamsikota3545:OaND13njPjqJtZyC@resumestore.av7rqbr.mongodb.net/")
+db = client["userregristation"]
+users_collection = db["register"]
 blacklist_collection = db["blacklisted_tokens"]
 
 blacklist_collection.create_index(
